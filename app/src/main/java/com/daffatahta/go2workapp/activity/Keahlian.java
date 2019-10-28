01c93edcb2b9f20f8b9666b11ffd5c99c08e85ff
firnandakusuma <firnandapratama99@gmail.com>
2019-10-28 23:05:48 +0700

    Layout Lanjutan

---
+++ app/src/main/java/com/daffatahta/go2workapp/activity/Keahlian.java
@@ -0,0 +1,16 @@
+package com.daffatahta.go2workapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.daffatahta.go2workapp.R;
+
+public class Keahlian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keahlian);
+    }
+}
