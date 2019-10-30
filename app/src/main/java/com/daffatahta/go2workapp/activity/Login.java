b41e40f1b4866ccea6d2b97c7f660875ca631e27
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-10-30 15:01:52 +0700

    login basic added

--- app/src/main/java/com/daffatahta/go2workapp/activity/Login.java
+++ app/src/main/java/com/daffatahta/go2workapp/activity/Login.java
@@ -1,26 +1,42 @@
 package com.daffatahta.go2workapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daffatahta.go2workapp.MainActivity;
 import com.daffatahta.go2workapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.r0adkll.slidr.Slidr;
 
 public class Login extends AppCompatActivity {
 
     private TextView toRegister;
     private TextView toForgotPassword;
+
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    private static final String TAG = "Login";
     private EditText username;
     private EditText password;
-
+    private Button login;
     @Override
     protected void onStart() {
         super.onStart();
@@ -31,6 +47,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Slidr.attach(this);
 
@@ -49,6 +68,16 @@ public class Login extends AppCompatActivity {
                 forgotPassword();
             }
         });
+
+        login = findViewById(R.id.buttonLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateInput();
+                login();
+
+            }
+        });
     }
 
     public void toRegister (){
@@ -58,6 +87,67 @@ public class Login extends AppCompatActivity {
 
     public void forgotPassword(){
         //Todo: make forgot password layout and class!
-
     }
+
+    public boolean validateInput(){
+        boolean bool = true;
+        username = findViewById(R.id.editTextUsernameLogin);
+        password = findViewById(R.id.editTextPasswordLogin);
+
+        if (username.getText().equals(0)){
+            username.setError("Username harus di isi!");
+            username.requestFocus();
+            return false;
+        }
+        if (password.getText().equals(0)){
+            password.setError("Password harus di isi!");
+            password.requestFocus();
+            return false;
+        }
+        return bool;
+    }
+
+    private void login(){
+        Log.d(TAG, "Login");
+        if (!validateInput()){
+            return;
+        }
+        String userN = username.getText().toString();
+        String passN = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(userN, passN).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signin" + task.isSuccessful());
+                if (task.isSuccessful()){
+                    Intent i  = new Intent(Login.this, MainActivity.class);
+                    finish();
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                    finish();
+                }
+                else {
+                    Toast.makeText(Login.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    //todo : here
+    //nagmbil tipe user
+    private void diff (final String tipe){
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(tipe).exists()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
